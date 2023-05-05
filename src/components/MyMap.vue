1edb0d3740dc4d9414e70773ec961c94ffb206f6
DuMaciel <edumaciel1221@gmail.com>
2023-05-05 15:46:25 -0300

    feat: creating simple map

---
+++ src/components/MyMap.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+// default projection is EPSG:3857, but to use latitude and longitude in degrees the projection must be EPSG:4326.
+const projection = ref('EPSG:4326');
+// coordinates are represented by array: [longitude, latitude].
+const center = ref([-53.711536836218585, -24.617732283197068]);
+// zoom ranges from 0 to 28, with 28 being as close as possible by default.
+const zoom = ref(8);
+// the rotation is defined as a circle perimeter, so it goes from 0 to 2PI, to rotate 90 degrees put the rotation in PI/2.
+const rotation = ref(0);
+
+</script>
+<template>
+    <ol-map
+    >
+      <ol-view
+        :projection="projection"
+        :center="center"
+        :zoom="zoom"
+        :rotation="rotation"
+      />
+      <ol-tile-layer>
+        <ol-source-osm />
+      </ol-tile-layer>
+      <ol-attribution-control
+        :collapsible="true"
+        tipLabel="Atribuições"
+      />
+      <ol-zoom-control
+        zoomInTipLabel="Aproximar"
+        zoomOutTipLabel="Afastar"
+      />
+      <ol-scaleline-control />
+      <ol-rotate-control 
+      tipLabel="Redefinir rotação para o norte"/>
+      <ol-fullscreen-control 
+      tipLabel="Alterar modo de tela cheia"/>
+      <ol-mouseposition-control/>
+      <ol-overviewmap-control 
+      tipLabel="Abrir mapa geral">
+        <ol-tile-layer>
+          <ol-source-osm />
+        </ol-tile-layer>
+      </ol-overviewmap-control>
+    </ol-map>
+</template>
+
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Vue3OpenLayers from 'vue3-openlayers'
 
-import './assets/main.css'
+import 'vue3-openlayers/dist/vue3-openlayers.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(Vue3OpenLayers)
+app.mount('#app')
